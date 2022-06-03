7c9acfc81f93e621f71bdd36b2c7b5fa6a4ce767
JUAN JOSE PEREZ LAZARTE <juanjo101619@gmail.com>
2022-06-02 22:27:25 -0500

    DOCS creado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import{ saludar} from './js/componentes.js';
 import './styles.css';
 
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+// import { Todo } from './classes/todo.class.js';
+// import { TodoList } from './classes/todo-list.class';
 
-const nombre = 'Juan';
+export const todoList = new TodoList();
 
-saludar (nombre);
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+//todoList.todos.forEach(crearTodoHtml);
+
+/* localStorage.setItem('mi-pinga', 'ABCDE');
+setTimeout(() =>{
+    
+    localStorage.removeItem('mi-pinga');
+}, 1500); */
+console.log( 'Arreglo de objetos' , todoList.todos);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,17 +1,98 @@
-import '../css/componentes.css';
-import webpacklogo from'../assets/img/webpack-logo.png';
+//Referencia al html
+import { Todo } from '../classes'
+import { todoList } from '../index'
 
 
-export const saludar = ( nombre ) =>{
-    console.log('Creando etiqueta h1');
+const divTodoList   = document.querySelector('.todo-list');
+const txtInput      = document.querySelector('.new-todo');
+const btnBorrar     = document.querySelector('.clear-completed');
+const ulFiltros     = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro');
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola , ${ nombre } !!!`;
+export const crearTodoHtml = ( todo ) =>{
+    const htmlTodo = `
+    <li class="${ (todo.completado) ? 'completed' : '' }" data-id="${ todo.id }">
+		<div class="view">
+			<input class="toggle" type="checkbox" ${(todo.completado) ? 'checked' : ''}>
+			<label>${ todo.tarea }</label>
+			<button class="destroy"></button>
+		</div>
+		<input class="edit" value="Create a TodoMVC template">
+	</li>`;
 
-    document.body.append(h1);
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
+    divTodoList.append( div.firstElementChild );
+    return div.firstElementChild;
 
-    //img 
-    // const img = document.createElement('img');
-    // img.src = webpacklogo;
-    // document.body.append(img);
 }
+
+//Eventos
+
+txtInput.addEventListener('keyup', (event) =>{
+    if(event.keyCode === 13 && txtInput.value.length > 0){
+        const nuevoTodo = new Todo( txtInput.value);
+        console.log(txtInput.value);
+        todoList.nuevoTodo( nuevoTodo );
+        crearTodoHtml(nuevoTodo);
+        txtInput.value = '';
+    }
+});
+
+divTodoList.addEventListener('click', (event) =>{
+    const nombreElemento = event.target.localName; // input, label, button
+    const todoElemento = event.target.parentElement.parentElement;
+    const todoId = todoElemento.getAttribute('data-id');
+
+    if( nombreElemento.includes('input')){
+
+        todoList.marcarCompletado( todoId);
+        todoElemento.classList.toggle('completed');
+
+    }else if( nombreElemento.includes('button')){
+
+        todoList.eliminarTodo( todoId );
+        divTodoList.removeChild( todoElemento);
+    }
+    console.log(todoList);
+});
+
+btnBorrar.addEventListener('click', () =>{
+    todoList.eliminarCompletados();
+    for(let i = divTodoList.children.length-1; i >=0 ; i-- ){
+        
+        const elemento = divTodoList.children[i];
+        if(elemento.classList.contains('completed')){
+            divTodoList.removeChild(elemento);
+        }
+    }
+});
+
+ulFiltros.addEventListener('click', (event) =>{
+    console.log( event.target.text );
+    const filtro = event.target.text;
+    if( !filtro ) {return;}
+
+    // anchorFiltros.forEach( elem.classList.remove('selected'));
+    anchorFiltros.forEach( elem => elem.classList.remove('selected'));
+    event.target.classList.add('selected');
+
+    for( const elemento of divTodoList.children){
+        elemento.classList.remove('hidden');
+        const completada = elemento.classList.contains('completed');
+
+        switch( filtro ){
+            case 'Pendientes':
+                if(completada){
+                    elemento.classList.add('hidden');
+                } 
+            break;
+
+            case 'Completados':
+                if( !completada ){
+                    elemento.classList.add('hidden');
+                }
+            break
+        }
+    }
+});
